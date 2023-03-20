2680ee3b1a628a6787c68faa72aeea149175adc5
gabrielcasag <gaspericasag@gmail.com>
2023-03-20 00:23:19 -0300

    🔇 remove some dummy code

--- background.js
+++ background.js
@@ -1,12 +1,4 @@
-let color = "#3aa757";
-
 // set an initial state or complete some tasks on installation
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({
-    themeActive: false,
-  });
-
-  // chrome.action.setBadgeText({
-  //   text: "OFF",
-  // });
+  chrome.storage.sync.set({ themeActive: false });
 });
